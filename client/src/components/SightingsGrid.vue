cb3829ac0822190bfe45ecef3a6c0d35a9c82a4d
User <user@Users-MacBook-Pro.local>
2020-02-26 09:02:58 +0000

    first commit

---
+++ client/src/components/SightingsGrid.vue
@@ -0,0 +1,54 @@
+<template lang="html">
+	<div id="sightingsGrid">
+		<sighting v-for="(sighting, index) in sightings" :keys="index" :sighting="sighting" />
+	</div>
+</template>
+
+<script>
+import Sighting from './Sighting';
+import SightingService from '../services/SightingService.js';
+import { eventBus } from '../main';
+export default {
+	name: "sightings-grid",
+	data () {
+    return {
+      sightings: []
+    }
+  },
+	components: {
+		'sighting': Sighting
+	},
+	mounted(){
+		SightingService.getSightings()
+	.then(sightings => this.sightings = sightings);
+
+	eventBus.$on('sighting-added', (sighting) => {
+		this.sightings.push(sighting)
+	})
+
+	eventBus.$on('sighting-deleted', (id) => {
+		let index = this.sightings.findIndex(sighting => sighting._id === id)
+		this.sightings.splice(index, 1)
+	})
+  },
+  methods: {
+
+  }
+
+}
+</script>
+
+<style lang="css" scoped>
+#sightingsGrid {
+	display: flex;
+	flex-wrap: wrap;
+	justify-content: space-evenly;
+}
+
+h2 {
+	padding: 0;
+	margin: 0;
+}
+
+
+</style>
